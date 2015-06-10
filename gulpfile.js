f213598bca6a70c7f2a0c7f787968e7e4b681462
Joshua Harris <joshua.jharris13@gmail.com>
2015-06-10 18:39:07 -0400

    misc

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-    includeSources = require('gulp-include-source');
-
-gulp.task('html', function() {
-  return gulp.src( './index.html' )
-    .pipe( includeSources() )
-    .pipe( gulp.dest('build/') );
-});
